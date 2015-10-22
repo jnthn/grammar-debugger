0f6f133ad786746f09ae0db334d4e1dc344b4a41
Tobias Leich <email@froggs.de>
2015-10-22 15:41:35 +0200

    unbreak the debugger
    A rakudo patch that is meant to make autothreading of junctions
    work on the LHS of a smartmatch forces us to care about Mu/Any.
    
    See: https://github.com/rakudo/rakudo/commit/5d83db

--- lib/Grammar/Tracer.pm
+++ lib/Grammar/Tracer.pm
@@ -10,6 +10,7 @@ my class TracedGrammarHOW is Metamodel::GrammarHOW {
     method find_method($obj, $name) {
         my $meth := callsame;
         return $meth if $meth.WHAT.^name eq 'NQPRoutine';
+        return $meth unless $meth ~~ Any;
         return $meth unless $meth ~~ Regex;
         return -> $c, |args {
             # Method name.
